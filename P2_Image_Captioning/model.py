d10ad4b01376866b3d0ae95693cf55100f4c688a
Melan <melan2308@gmail.com>
2018-05-21 19:16:15 +0200

    Create hidden state initializer method and create embedding layer in DecoderRNN constructor

--- P2_Image_Captioning/model.py
+++ P2_Image_Captioning/model.py
@@ -20,8 +20,17 @@ class EncoderCNN(nn.Module):
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size):
+        ''' Initialize the layers of this model.'''
         super(DecoderRNN, self).__init__()
         
+        # Keep track of hidden_size for initialization of hidden state
+        self.hidden_size = hidden_size
+        
+        # Embedding layer that turns words into a vector of a specified size
+        self.word_embeddings = nn.Embedding(vocab_size, embed_size)
+        
+        # The LSTM takes embedded word vectors (of a specified size) as input
+        # and outputs hidden states of size hidden_dim
         self.lstm = nn.LSTM(input_size=embed_size, \
                             hidden_size=hidden_size, # LSTM hidden units 
                             num_layers=1, # number of LSTM layer
@@ -31,8 +40,13 @@ class DecoderRNN(nn.Module):
                             bidirectional=False, # unidirectional LSTM
                            )
         
+        # The linear layer that maps the hidden state output dimension
+        # to the number of words we want as output, vocab_size
         self.linear = nn.Linear(hidden_size, vocab_size)                     
 
+        # initialize the hidden state
+        self.hidden = self.init_hidden()
+        
     def forward(self, features, captions):
         
 
