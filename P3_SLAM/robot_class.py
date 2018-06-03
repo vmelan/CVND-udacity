e03f128684a459bbdcc04480656713332ffd24b5
Melan <melan2308@gmail.com>
2018-06-03 22:15:52 +0200

    Update robot class script with correct calls on class methods

--- P3_SLAM/robot_class.py
+++ P3_SLAM/robot_class.py
@@ -89,21 +89,22 @@ class robot:
         ## 3. If either of the distances, dx or dy, fall outside of the internal var, measurement_range
         ##    then we cannot record them; if they do fall in the range, then add them to the measurements list
         ##    as list.append([index, dx, dy]), this format is important for data creation done later
+
         dx, dy = 0, 0
 
-        for landmark_index in range(num_landmarks):
-        	# Compute dx and dy
-        	dx, dy = r.landmarks[landmark_index][0] - self.x, r.landmarks[landmark_index][1] - self.y
-        	# Adding noise
-        	noise = self.rand() * self.measurement_noise
-        	dx, dy = dx + noise, dy + noise
-        	# Check if valid values
-	        if dx > self.measurement_range or dy > self.measurement_range:
-	            raise ValueError("Cannot record the values")
-	        # Keep track of landmark_index, dx, dy in measurements list
-	        measurements.append([landmark_index, dx, dy])
-
-
+        for landmark_index in range(self.num_landmarks):
+            # Compute dx and dy
+            dx, dy = self.landmarks[landmark_index][0] - self.x, self.landmarks[landmark_index][1] - self.y
+            # Adding noise
+            noise = self.rand() * self.measurement_noise
+            dx, dy = dx + noise, dy + noise
+            # Check if we can sense the landmark
+            if dx > self.measurement_range or dy > self.measurement_range:
+                continue
+            else:
+                # Keep track of landmark_index, dx, dy in measurements list
+                measurements.append([landmark_index, dx, dy])        
+        
         ## TODO: return the final, complete list of measurements
         return measurements
 
